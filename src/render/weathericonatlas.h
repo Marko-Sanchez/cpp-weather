d70e526ff150a0920871ecf9e79b712382e9c30c
Marko-Sanchez <sanchezmarco0909@gmail.com>
2026-04-24 00:09:22 -0700

    Updated to use better looking weather icons; icons in atlas are organized allowing for more structured indexing.

--- src/render/weathericonatlas.h
+++ src/render/weathericonatlas.h
@@ -22,19 +22,20 @@ private:
 public:
 
     WeatherIconAtlas():
-        m_atlas(LoadTexture("resources/images/weather-icon-atlas.png")),
-        m_iconSize(32.0f),
-        m_columns(4)
+        m_atlas(LoadTexture("resources/images/weatheratlas64x.png")),
+        m_iconSize(64.0f),
+        m_columns(5)
     {
-        m_iconPositions[utility::WeatherCondition::Clear]        = {2, 3};
-        m_iconPositions[utility::WeatherCondition::PartlyCloudy] = {1, 3};
-        m_iconPositions[utility::WeatherCondition::Cloudy]       = {0, 6};
+        m_iconPositions[utility::WeatherCondition::Clear]        = {0, 0};
+        m_iconPositions[utility::WeatherCondition::Foggy]        = {0, 1};
+        m_iconPositions[utility::WeatherCondition::PartlyCloudy] = {0, 2};
+        m_iconPositions[utility::WeatherCondition::Cloudy]       = {0, 3};
+        m_iconPositions[utility::WeatherCondition::Drizzle]      = {0, 4};
         m_iconPositions[utility::WeatherCondition::Rainy]        = {1, 0};
         m_iconPositions[utility::WeatherCondition::RainShowers]  = {1, 1};
-        m_iconPositions[utility::WeatherCondition::Snowy]        = {2, 2};
-        m_iconPositions[utility::WeatherCondition::Drizzle]      = {0, 0};
-        m_iconPositions[utility::WeatherCondition::Foggy]        = {0, 1};
-        m_iconPositions[utility::WeatherCondition::Unknown]      = {3, 3};
+        m_iconPositions[utility::WeatherCondition::Snowy]        = {1, 2};
+        m_iconPositions[utility::WeatherCondition::ThunderStorm] = {1, 3};
+        m_iconPositions[utility::WeatherCondition::Unknown]      = {1, 4};
     }
 
     ~WeatherIconAtlas()
@@ -42,16 +43,24 @@ public:
         UnloadTexture(m_atlas);
     }
 
-    void DrawWeatherIcon(utility::WeatherCondition condition, Vector2 pos, float scale, Color tint = WHITE)
+    float GetIconSize() const noexcept
+    {
+        return m_iconSize;
+    }
+
+    void DrawWeatherIcon(const utility::WeatherCondition condition, const Vector2 pos, const float scale, const Color tint = WHITE) const
     {
         if (!m_iconPositions.contains(condition))
         {
             return;
         }
 
-        const auto& [row, col] = m_iconPositions[condition];
+        const auto& [row, col] = m_iconPositions.at(condition);
 
-        Rectangle source
+        const Vector2 origin{0.0f, 0.0f};
+        float rotation{0.0f};
+
+        const Rectangle source
         {
             col * m_iconSize,
             row * m_iconSize,
@@ -59,7 +68,7 @@ public:
             m_iconSize
         };
 
-        Rectangle dest
+        const Rectangle dest
         {
             pos.x,
             pos.y,
@@ -67,7 +76,7 @@ public:
             m_iconSize * scale
         };
 
-        DrawTexturePro(m_atlas, source, dest, Vector2{0.0f, 0.0f}, 0.0f, tint);
+        DrawTexturePro(m_atlas, source, dest, origin, rotation, tint);
     }
 };
 }// namespace render
