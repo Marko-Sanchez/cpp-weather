f733ca18d1b2d803ed7c12659c463e8d1fe565cc
Marko-Sanchez <sanchezmarco0909@gmail.com>
2026-05-20 18:53:16 -0700

    Added ability to draw round panels.

--- src/render/paneldecorator.cpp
+++ src/render/paneldecorator.cpp
@@ -4,15 +4,23 @@
 
 namespace render
 {
-PanelDecorator::PanelDecorator(std::unique_ptr<DrawableComponent>&& component, Color color)
+PanelDecorator::PanelDecorator(std::unique_ptr<DrawableComponent>&& component, const Color color, const bool isRounded)
     :ComponentDecorator(std::move(component)),
-    _color(color)
+    _color(color),
+    _isRounded(isRounded)
 {}
 
 void PanelDecorator::OnRender(const float scrollOffset) const
 {
     auto panel = _component->GetBounds(scrollOffset);
-    DrawRectangleRec(panel, _color);
+    if (_isRounded)
+    {
+        DrawRectangleRounded(panel, _panelRoundness, _panelSegments, _color);
+    }
+    else
+    {
+        DrawRectangleRec(panel, _color);
+    }
 
     _component->OnRender(scrollOffset);
 }
--- src/render/paneldecorator.h
+++ src/render/paneldecorator.h
@@ -12,11 +12,15 @@ class PanelDecorator: public ComponentDecorator
 {
     private:
 
+        const float _panelRoundness {0.2f};
+        const int   _panelSegments  {0};
+
         Color _color;
+        bool  _isRounded;
 
     public:
 
-        PanelDecorator(std::unique_ptr<DrawableComponent>&& component, Color color);
+        PanelDecorator(std::unique_ptr<DrawableComponent>&& component, const Color color, const bool isRounded = false);
 
         void OnRender(const float scrollOffset) const override;
 
